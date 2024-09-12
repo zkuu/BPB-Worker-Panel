1bd7d5db4233ba2d263561321bcc95577b17eaae
zkuu <47938258+zkuu@users.noreply.github.com>
2024-09-12 09:09:38 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -219,7 +219,7 @@ export default {
 
                     default:
                         return new Response('Not found', { status: 404 });
-                        url.hostname = 'www.speedtest.net';
+                        url.hostname = 'v2.hwfzku.dynv6.net';
                         url.protocol = 'https:';
                         request = new Request(url, request);
                         return await fetch(request);
